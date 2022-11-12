fe35d68fa4c31e18ab1b1bc00e5f50ea9ce466cc
Maëlick Claes <maelick.claes@mobilaris.se>
2022-11-12 13:06:37 +0100

    NetworkManager

--- Group11/Assets/scripts/Handlers/NetworkManager.cs
+++ Group11/Assets/scripts/Handlers/NetworkManager.cs
@@ -1,8 +1,173 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NetworkManager
 {
-    
+    private const int Port = 4242;
+    private static GameServer _server;
+    private static GameClient _client;
+    public static readonly Queue<string> Queue = new();
+
+    public static void Start(string serverHost)
+    {
+        if (_client != null) return;
+        if (serverHost == null)
+        {
+            serverHost = Dns.GetHostName();
+            _server = new(serverHost, Port);
+            _server.Run();
+        }
+
+        _client = new GameClient(serverHost, Port);
+        _client.Run();
+    }
+
+    public static IPAddress GetIpAddress(string host)
+    {
+        IPHostEntry ipHostInfo = Dns.GetHostEntry(host);
+        foreach (var ip in ipHostInfo.AddressList)
+        {
+            if (ip.AddressFamily ==
+                AddressFamily.InterNetwork)
+            {
+                return ip;
+            }
+        }
+
+        return null;
+    }
+
+    public static void SendMove(Vector2 moveInput)
+    {
+        if (_client != null)
+        {
+            _client.Send(moveInput.ToString());
+        }
+    }
+}
+
+public class GameClient
+{
+    private readonly IPAddress _serverIp;
+    private readonly int _serverPort;
+    private TcpClient _client;
+    private StreamWriter _writer;
+    private StreamReader _reader;
+
+    public GameClient(string host, int port)
+    {
+        this._serverIp = NetworkManager.GetIpAddress(host);
+        this._serverPort = port;
+    }
+
+    public async Task Connect()
+    {
+        if (_serverIp == null)
+            throw new Exception("No IPv4 found for server");
+        _client = new TcpClient();
+        await _client.ConnectAsync(_serverIp, _serverPort);
+        Debug.Log("Client connected to " + _serverIp);
+        NetworkStream networkStream = _client.GetStream();
+        _writer = new StreamWriter(networkStream);
+        _reader = new StreamReader(networkStream);
+        _writer.AutoFlush = true;
+    }
+
+    public async Task Send(string message)
+    {
+        if (_writer != null)
+        {
+            await _writer.WriteLineAsync(message);
+            Debug.Log("Message sent to server");
+        }
+    }
+
+    public async Task Run()
+    {
+        await Connect();
+        while (true)
+        {
+            var response = await _reader.ReadLineAsync();
+            if (response != null)
+            {
+                Debug.Log("Message from server added to queue: " + response);
+                NetworkManager.Queue.Enqueue(response);
+            }
+            else
+                break;
+        }
+    }
+}
+
+public class GameServer
+{
+    private readonly IPAddress _ipAddress;
+    private readonly int _port;
+
+    public GameServer(string host, int port)
+    {
+        this._port = port;
+        this._ipAddress = NetworkManager.GetIpAddress(host);
+        if (this._ipAddress == null)
+            throw new Exception("No IPv4 address for server");
+    }
+
+    public async Task Run()
+    {
+        TcpListener listener = new TcpListener(this._ipAddress, this._port);
+        listener.Start();
+        Debug.Log("Game server is now running on port " + this._port);
+        while (true) {
+            try {
+                TcpClient tcpClient = await listener.AcceptTcpClientAsync();
+                Task t = Process(tcpClient);
+                await t;
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    private async Task Process(TcpClient tcpClient)
+    {
+        string clientEndPoint =
+            tcpClient.Client.RemoteEndPoint.ToString();
+        Debug.Log("Received connection request from " + clientEndPoint);
+        try {
+            NetworkStream networkStream = tcpClient.GetStream();
+            StreamReader reader = new StreamReader(networkStream);
+            StreamWriter writer = new StreamWriter(networkStream);
+            writer.AutoFlush = true;
+            while (true) {
+                string request = await reader.ReadLineAsync();
+                if (request != null) {
+                    Debug.Log("Received message: " + request);
+                    string response = Response(request);
+                    Debug.Log("Response is: " + response);
+                    await writer.WriteLineAsync(response);
+                }
+                else
+                    break; // Client closed connection
+            }
+            tcpClient.Close();
+        }
+        catch (Exception ex) {
+            Debug.Log(ex.Message);
+            if (tcpClient.Connected)
+                tcpClient.Close();
+        }
+    }
+
+    private static string Response(string request)
+    {
+        return request;
+    }
 }
